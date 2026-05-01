2bb7ff73977c48ec4da3e974f17dfb873bf0bbaa
Ashisharjun12 <ashishrahul748@gmail.com>
2026-05-01 14:34:06 +0530

    update viteconfig

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vite'
+import { defineConfig , loadEnv} from 'vite'
 import react, { reactCompilerPreset } from '@vitejs/plugin-react'
 import babel from '@rolldown/plugin-babel'
 import tailwindcss from '@tailwindcss/vite'
@@ -19,12 +19,6 @@ import path from 'path'
 //     },
 //   },
 // })
-
-
-import path from "path";
-import { defineConfig, loadEnv } from "vite";
-import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
 
 export default ({ mode }) => {
   // load env variables based on mode (development / production)
