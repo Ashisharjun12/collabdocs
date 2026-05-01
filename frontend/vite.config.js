38132a2213f4e8db430558f4f8106483f6741dd1
Ashisharjun12 <ashishrahul748@gmail.com>
2026-05-01 14:41:57 +0530

    update viteconfig

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,4 +1,4 @@
-import { defineConfig , loadEnv} from 'vite'
+import { defineConfig } from 'vite'
 import react, { reactCompilerPreset } from '@vitejs/plugin-react'
 import babel from '@rolldown/plugin-babel'
 import tailwindcss from '@tailwindcss/vite'
@@ -6,48 +6,19 @@ import path from 'path'
 
 
 // // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [
-//     react(),
-//     babel({ presets: [reactCompilerPreset()] }),
-//     tailwindcss(),
+export default defineConfig({
+  plugins: [
+    react(),
+    babel({ presets: [reactCompilerPreset()] }),
+    tailwindcss(),
     
-//   ],
-//     resolve: {
-//     alias: {
-//       "@": path.resolve(__dirname, "./src"),
-//     },
-//   },
-// })
-
-export default ({ mode }) => {
-  // load env variables based on mode (development / production)
-  const env = loadEnv(mode, process.cwd(), "");
-
-  return defineConfig({
-    plugins: [
-      react(),
-   babel({ presets: [reactCompilerPreset()] }),
-   tailwindcss(),
-
-    ],
-
+  ],
     resolve: {
-      alias: {
-        "@": path.resolve(__dirname, "./src"),
-      },
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
     },
+  },
+})
 
-    server: {
-      proxy: {
-        "/api": {
-          target: env.VITE_APP_URL,
-          changeOrigin: true,
-          rewrite: (path) => path.replace(/^\/api/, "/api/v1"),
-          secure: true,
-        },
-      },
-    },
-  });
-};
+
 
