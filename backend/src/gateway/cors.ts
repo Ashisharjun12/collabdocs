477d01cae84db9297a8da2b28b3c84eb83355f09
Ashisharjun12 <ashishrahul748@gmail.com>
2026-05-01 14:43:08 +0530

    fix cors

--- backend/src/gateway/cors.ts
+++ backend/src/gateway/cors.ts
@@ -1,8 +1,8 @@
 
 export const corsOption = {
-    origin: ["https://collabdocs-roan.vercel.app"],
+    origin: ["https://collabdocs-roan.vercel.app", "http://localhost:5173"],
     credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS" ],
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization", "X-Requested-With"],
     preflightContinue: false,
     optionsSuccessStatus: 204,
